dc6e5808b4913227797704cbe276ba8dcc4b2cd3
MD Shakib <shakibahmed.528874@gmail.com>
2023-07-12 18:15:18 +0600

    updated blog page and navigation

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const menuIcon = document.getElementById('menu_icon');
 const menu = document.getElementById('menu');
 
 menuIcon.addEventListener('click', () => {
-    if (menu.className === 'hidden') {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
+  if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden');
+    menuIcon.classList.add('fa-times');
+    menuIcon.classList.remove('fa-bars');
+  } else {
+    menu.classList.add('hidden');
+    menuIcon.classList.add('fa-bars');
+    menuIcon.classList.remove('fa-times');
+  }
+  menu.classList.add('animation-class'); // Add animation class
 });
-
-
