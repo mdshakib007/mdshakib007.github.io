aee4555e7a5d69d386fc77a3cfefd9b4bc564538
MD Shakib <shakibahmed.528874@gmail.com>
2023-07-19 18:43:43 +0600

    updated navbar annimation

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ const menu = document.getElementById('menu');
 menuIcon.addEventListener('click', () => {
   if (menu.classList.contains('hidden')) {
     menu.classList.remove('hidden');
+    menu.classList.add('navbar-open-animation');
     menuIcon.classList.add('fa-times');
     menuIcon.classList.remove('fa-bars');
+    menu.classList.remove('navbar-close-animation');
   } else {
+    menu.classList.add('navbar-close-animation');
+    menu.classList.remove('navbar-open-animation');
     menu.classList.add('hidden');
     menuIcon.classList.add('fa-bars');
     menuIcon.classList.remove('fa-times');
   }
-  menu.classList.add('animation-class'); // Add animation class
 });
